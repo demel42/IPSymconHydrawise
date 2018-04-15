c490d994047ac4b66899504c463489adc0e6e46c
demel42 <demel42@web.de>
2018-04-15 17:04:40 +0200

    Anpassung der Ausgabe des vibility_scripts

--- libs/HydrawiseVisibility.php
+++ libs/HydrawiseVisibility.php
@@ -1,33 +1,33 @@
-<?php
+<?
 
 $instID = $_IPS['InstanceID'];
 
 $cfg = json_decode(IPS_GetConfiguration($instID), true);
 $with_daily_value = $cfg['with_daily_value'];
 
-$duration = $_IPS['duration'];
+$next_duration = $_IPS['next_duration'];
 $time_left = $_IPS['time_left'];
 $suspended_until = $_IPS['suspended_until'];
 
-$msg = 'duration=' . $duration . ', time_left=' . $time_left . ', suspended_until=' . $suspended_until;
+$msg = ''; //'next_duration=' . $next_duration . ', time_left=' . $time_left . ', suspended_until=' . $suspended_until;
 $msg_e = [];
 $msg_v = [];
 $msg_h = [];
 
-$triggerVars = ['duration', 'run', 'daily'];
+$triggerVars = ['next_duration', 'run', 'daily'];
 foreach ($triggerVars as $triggerVar) {
     switch ($triggerVar) {
-        case 'duration':
-            $hideVars = ['Duration', 'Duration_seconds'];
-            $do_hide = !($duration > 0 && $suspended_until == 0);
+        case 'next_duration':
+            $hideVars = ['NextDuration', 'NextDuration_seconds'];
+            $do_hide = ! ($next_duration > 0 && $suspended_until == 0);
             break;
         case 'run':
             $hideVars = ['TimeLeft', 'WaterUsage'];
-            $do_hide = !($time_left > 0);
+            $do_hide = ! ($time_left > 0);
             break;
         case 'daily':
             $hideVars = ['DailyDuration', 'DailyDuration_seconds', 'DailyWaterUsage'];
-            $do_hide = !($with_daily_value && $suspended_until == 0);
+            $do_hide = ! ($with_daily_value && $suspended_until == 0);
             break;
         default:
             $vars = [];
@@ -54,11 +54,13 @@ if ($msg_e != []) {
 }
 if ($msg_v != []) {
     $msg .= $msg != '' ? '; ' : '';
-    $msg .= 'set visible: ' . implode(', ', $msg_v);
+    $msg .= 'visible=' . implode(', ', $msg_v);
 }
 if ($msg_h != []) {
     $msg .= $msg != '' ? '; ' : '';
-    $msg .= 'set hidden: ' . implode(', ', $msg_h);
+    $msg .= 'hidden=' . implode(', ', $msg_h);
 }
 
 echo $msg . "\n";
+
+?>
