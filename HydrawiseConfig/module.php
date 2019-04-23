aa428f32130e6ace489a8d6cf9dba1934721a392
demel42 <demel42@web.de>
2019-04-23 15:24:39 +0000

    Apply fixes from StyleCI

--- HydrawiseConfig/module.php
+++ HydrawiseConfig/module.php
@@ -59,11 +59,11 @@ class HydrawiseConfig extends IPSModule
         $formActions = [];
         $formActions[] = ['type' => 'Select', 'name' => 'controller_id', 'caption' => 'Controller', 'options' => $options];
         $formActions[] = [
-							'type'    => 'Button',
-							'caption' => 'Import of controller',
-							'confirm' => 'Triggering the function creates the missing instances for the base-unit and all zones and sensors of the selected Hydrawise-Controller. Are you sure?',
-							'onClick' => 'HydrawiseConfig_Doit($id, $controller_id);'
-						];
+                            'type'    => 'Button',
+                            'caption' => 'Import of controller',
+                            'confirm' => 'Triggering the function creates the missing instances for the base-unit and all zones and sensors of the selected Hydrawise-Controller. Are you sure?',
+                            'onClick' => 'HydrawiseConfig_Doit($id, $controller_id);'
+                        ];
         $formActions[] = ['type' => 'Label', 'label' => '____________________________________________________________________________________________________'];
         $formActions[] = [
                             'type'    => 'Button',
